dc47564b570d491546fe7a8748ce67e5a0c9a106
dahiyakapil <kapildahiya309@gmail.com>
2025-12-28 22:26:21 +0530

    create getAllProducts, getProductById, updateProduct

--- server/src/controllers/products.controller.js
+++ server/src/controllers/products.controller.js
@@ -16,3 +16,62 @@ export const createProduct = async (req, res) => {
         )
     }
 }
+
+export const getAllProducts = async (req, res) => {
+    try {
+        const allProducts = await Product.find();
+        res.status(200).json({
+            message: "Products fetched successfully",
+            data: allProducts
+        })
+    } catch (error) {
+        return res.status(500).json(
+            {
+                message: "Server Error",
+                error: error.message
+            }
+        )
+    }
+}
+
+
+export const getProductById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const findProduct = await Product.findById(id);
+        res.status(200).json({
+            message: "Product fetched successfully",
+            data: findProduct
+        })
+    } catch (error) {
+        return res.status(500).json(
+            {
+                message: "Server Error",
+                error: error.message
+            }
+        )
+    }
+}
+
+
+export const updateProduct = async (req, res) => {
+    try {
+        // Take id from req.params
+        const { id } = req.params;
+
+        // Find the product by Id and update
+        const newUpdatedPoduct = await Product.findByIdAndUpdate(id, req.body, { new: true });
+
+        res.status(200).json({
+            message: "Product updated successfully",
+            data: newUpdatedPoduct
+        })
+    } catch (error) {
+        return res.status(500).json(
+            {
+                message: "Server Error",
+                error: error.message
+            }
+        )
+    }
+}
--- server/src/routes/products.route.js
+++ server/src/routes/products.route.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createProduct } from "../controllers/products.controller.js";
+import { createProduct, getAllProducts, getProductById, updateProduct } from "../controllers/products.controller.js";
 import { authMiddleware, isAdmin } from "../middleware/auth.middleware.js";
 
 const productsRouter = express.Router();
 
-productsRouter.post("/create-product", authMiddleware, isAdmin, createProduct)
+productsRouter.post("/create-product", authMiddleware, isAdmin, createProduct);
+productsRouter.get("/all-products", authMiddleware, isAdmin, getAllProducts);
+productsRouter.get("/:id", authMiddleware, isAdmin, getProductById);
+productsRouter.put("/:id", authMiddleware, isAdmin, updateProduct);
+
+
 
 export default productsRouter;
